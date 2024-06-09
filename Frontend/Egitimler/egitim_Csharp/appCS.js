b9a149df4666d5f3602f208ed18bf4f236410ec3
Eyüp DEMİR <eyupdmir50@gmail.com>
2024-06-09 16:05:02 +0300

    C# eğitimine alıştırma ekranı dinamik olarak eklendi

--- Frontend/Egitimler/egitim_Csharp/appCS.js
+++ Frontend/Egitimler/egitim_Csharp/appCS.js
@@ -7,3 +7,23 @@ h2Etiket.onclick = function() {
 
 h2Etiket.style.cursor = 'pointer';
 h2Etiket.title = 'AnaSayfa';
+
+
+function checkAnswer() {
+    var userInput = document.getElementById("userInput").value.trim();
+    var expectedAnswer = 'Console.WriteLine("Hello World");';
+    var resultElement = document.getElementById("result");
+    var inputElement = document.getElementById("userInput");
+
+    if (userInput === expectedAnswer) {
+        resultElement.textContent = "Tebrikler! Doğru cevap.";
+        resultElement.style.color = "green";
+        inputElement.style.backgroundColor = "lightgreen";
+    } 
+    else 
+    {
+        resultElement.textContent = "Maalesef, yanlış cevap. Lütfen tekrar deneyin.";
+        resultElement.style.color = "red";
+        inputElement.style.backgroundColor = "red";
+    }
+}
