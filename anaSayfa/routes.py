e2d486dad82b465be0368b543843005ec3224d66
Eyüp DEMİR <eyupdmir50@gmail.com>
2024-06-19 04:27:26 +0300

    Kullanıcı İşlem Hataları Düzeltildi, Dosya İsimlerindeki Olası Karışıklıklar Giderildi.

--- anaSayfa/routes.py
+++ anaSayfa/routes.py
@@ -1,4 +1,3 @@
-from flask import Blueprint, render_template
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 import json
 import os
@@ -6,11 +5,6 @@ from datetime import datetime
 
 anaSayfa_bp = Blueprint('anaSayfa', __name__, template_folder='templates', static_folder='static')
 data_file = 'users.json'
-
-@anaSayfa_bp.route('/')
-def index():
-    return render_template('index.html')
-
 
 # Kullanıcı verilerini saklamak için dosya adı
 def load_users():
@@ -24,6 +18,9 @@ def save_users(users):
     with open(data_file, 'w') as file:
         json.dump(users, file, indent=4)
 
+@anaSayfa_bp.route('/')
+def index():
+    return render_template('index.html')
 
 @anaSayfa_bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,9 +31,13 @@ def login():
         
         if username in users and users[username]['password'] == password:
             flash(f"Hoşgeldiniz, {users[username]['name']} {users[username]['surname']}", "success")
-            return redirect(url_for('anaSayfa.index'))
+            return f"""
+                <script>
+                    window.alert("Sayın {username} Hoşgeldiniz.."); window.location.href = "{url_for('anaSayfa.index')}";
+                </script>"""
         else:
             flash("Kullanıcı adı veya şifre hatalı.", "danger")
+            return redirect(url_for('anaSayfa.login_alert', message="Kullanıcı adı veya şifre hatalı."))
     
     return render_template('login.html')
 
@@ -53,13 +54,13 @@ def signup():
         
         if username in users:
             flash("Bu kullanıcı adı zaten alınmış.", "danger")
-            return redirect(url_for('anaSayfa.signup'))
+            return redirect(url_for('anaSayfa.signup_alert', message="Bu kullanıcı adı zaten alınmış."))
         
         try:
             datetime.strptime(birthdate, "%Y-%m-%d")
         except ValueError:
-            flash("Geçersiz doğum tarihi formatı. Lütfen YYYY-MM-DD formatında giriniz.", "danger")
-            return redirect(url_for('anaSayfa.signup'))
+            flash("Geçersiz doğum tarihi formatı. Lütfen YYYY-MM-DD formatında ve gerçekçi tarih bilgileri giriniz.", "danger")
+            return redirect(url_for('anaSayfa.signup_alert', message="Geçersiz doğum tarihi formatı. Lütfen YYYY-MM-DD formatında ve gerçekçi tarih bilgileri giriniz."))
         
         users[username] = {
             'name': name,
@@ -70,6 +71,26 @@ def signup():
         
         save_users(users)
         flash("Kayıt başarılı. Giriş yapabilirsiniz.", "success")
-        return redirect(url_for('anaSayfa.login'))
+        return f"""
+            <script>
+                window.alert("Sayın {username} Hoşgeldiniz..");
+                window.location.href = "{url_for('anaSayfa.index')}";
+            </script>
+            {redirect(url_for('anaSayfa.login'))}
+            """
     
     return render_template('signup.html')
+
+@anaSayfa_bp.route('/login_alert')
+def login_alert():
+    message = request.args.get('message', '')
+    return f"""
+    <script>window.alert("{message}"); window.location.href = "{url_for('anaSayfa.login')}";</script>
+    """
+
+@anaSayfa_bp.route('/signup_alert')
+def signup_alert():
+    message = request.args.get('message', '')
+    return f"""
+    <script>window.alert("{message}"); window.location.href = "{url_for('anaSayfa.signup')}";</script>
+    """
